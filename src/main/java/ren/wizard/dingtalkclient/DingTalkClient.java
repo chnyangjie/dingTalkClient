0b98b4e8a91c1b8708676f995723253fa6019b1a
chnyangjie <chnyangjie@163.com>
2018-04-17 10:05:25 +0800

    add proxy support

--- src/main/java/ren/wizard/dingtalkclient/DingTalkClient.java
+++ src/main/java/ren/wizard/dingtalkclient/DingTalkClient.java
@@ -3,11 +3,17 @@ package ren.wizard.dingtalkclient;
 
 import com.google.gson.Gson;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import ren.wizard.dingtalkclient.message.DingMessage;
@@ -29,13 +35,36 @@ public class DingTalkClient {
     public static DingTalkClient getInstance() {
         return ourInstance;
     }
-
-    public CloseableHttpClient getHttpclient() {
-        return httpclient;
-    }
-
-    public void setHttpclient(CloseableHttpClient httpclient) {
-        this.httpclient = httpclient;
+    /**
+     * send message
+     *
+     * @param webhook ding talk webhook or access token
+     * @param message ding talk message {@link DingMessage}
+     * @param proxy
+     * @param port
+     * @param username
+     * @param password
+     * @return
+     * @throws IOException
+     */
+    public SendResult sendMessage(String webhook, DingMessage message, String proxy, int port, String username, String password) throws IOException {
+        if (StringUtils.isBlank(proxy) && port <= 0) {
+            return sendMessage(webhook, message);
+        } else {
+            HttpClientBuilder builder = HttpClients.custom();
+            HttpHost httpHost = new HttpHost(proxy, port);
+            if (!StringUtils.isBlank(username)) {
+                CredentialsProvider credsProvider = new BasicCredentialsProvider();
+                credsProvider.setCredentials(
+                        new AuthScope(proxy, port),
+                        new UsernamePasswordCredentials(username, password)
+                );
+                builder.setDefaultCredentialsProvider(credsProvider);
+            }
+            builder.setProxy(httpHost);
+            httpclient = builder.build();
+            return sendMessage(webhook, message);
+        }
     }
 
     /**
